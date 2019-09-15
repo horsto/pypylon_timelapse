2806336286c8f94d62298312cab0b45cf7c7bdcb
Horst Obenhaus <horst.obenhaus@ntnu.no>
2019-09-15 20:25:14 +0200

    Make movie routine

---
+++ make_movie_last_day.py
@@ -0,0 +1,26 @@
+# Make a timelapse video from the last day
+
+from datetime import datetime,timedelta
+from dj_schemas import *
+import imageio
+from PIL import Image, ImageFont, ImageDraw
+
+similarity_threshold = .92
+output_basefolder = '/mnt/N/horsto/timelapses'
+
+if __name__ == "__main__":
+    print('############ Make movie ############')
+    # Collect output from the last day
+    restrictor = (timelapse.TimeLapse * timelapse.Similarity & 'ssim < {}'.format(similarity_threshold) \
+                  & 'entry_time_picture > "{}"'.format(datetime.today() - timedelta(days=1)))
+    yesterday = datetime.strftime(datetime.today() - timedelta(days=1),'%d_%m_%Y') # For movie filename
+    
+    # Write mp4 file
+    with imageio.get_writer('{}/{}.mp4'.format(output_basefolder, yesterday), fps=15) as writer:
+        for pic in restrictor:
+            img = Image.fromarray(pic['picture']) # Transform into PIL object
+            draw = ImageDraw.Draw(img)
+            font = ImageFont.truetype('Fernando.otf', 115)
+            draw.text((50,50),'{}'.format(datetime.strftime(pic['entry_time_picture'],'%d.%m.%Y')),(255,255,255),font=font)
+            draw.text((50,150),'{}'.format(datetime.strftime(pic['entry_time_picture'],'%H:%M:%S')),(255,255,255),font=font)
+            writer.append_data(np.asarray(img))
